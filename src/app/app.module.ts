12f7c90568a2c8cabb08880351d6aa2925b12335
Julian Carrasco <jecc_8212@gmail.com>
2023-07-16 23:23:51 -0400

    fin de seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/Heroes.module';
+import { CounterModule } from './counter/counter.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
